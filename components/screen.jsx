5f460c65aad241f7c8143f0c61f003e42c79f7ab
Wayne Zhu <way87767@gmail.com>
2020-11-21 00:43:08 -0500

    initialize RN app

---
+++ components/screen.jsx
@@ -0,0 +1,17 @@
+import generateLayout from "../lib/layout.js";
+import { StyleSheet, Text, View } from 'react-native';
+
+const Screen = () => {
+  const layout = generateLayout();
+  for(const row of layout){
+    
+  }
+
+  return(
+    <View>
+        
+    </View>
+  )
+}
+
+export default Screen;
